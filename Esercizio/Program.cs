d965961d141f0cdaf5273f102987db5dafd4beb3
marco <gundul1@libero.it>
2018-11-15 10:30:41 +0100

    fine esercizio

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -6,12 +6,81 @@ namespace Esercizio
     {
         static void Main(string[] args)
         {
-            string[,] matrice = new string[10, 10];
-            for(int i=0; i<10; i++)
+            int righe = 9;
+            int colonne = 9;
+            Boolean morto = false;
+            char tasto;
+            Car[] macchine = new Car[7];
+            for(int i=0; i<7; i++)
             {
-                for(int j=0; j<10; j++)
+                macchine[i] = new Car();
+            }
+            string[,] matrice = new string[righe, colonne];
+            for(int i=0; i<righe; i++)
+            {
+                for(int j=0; j<colonne; j++)
                 {
                     matrice[i,j] = " - ";
+                }
+            }
+            Persona pg = new Persona((righe - 1), (colonne - 1));
+            do {
+                pg.showPersona(matrice);
+                stampaMappa(matrice, righe, colonne);
+                Console.WriteLine("Scegli come muoverti con WASD");
+                tasto = Console.ReadKey().KeyChar;
+                Console.WriteLine(tasto);
+                matrice[pg.y, pg.x] = " - ";
+                switch(tasto)
+                {
+                    case 'w':
+                        pg.y--;
+                        break;
+                    case 's':
+                        pg.y++;
+                        break;
+                    case 'a':
+                        pg.x--;
+                        break;
+                    case 'd':
+                        pg.x++;
+                        break;
+                    default:
+                        Console.WriteLine("Non è passato in nessuno dei casi");
+                        break;
+                }
+                foreach(Car macchina in macchine)
+                {
+                    int vx = macchina.x;
+                    int vy = macchina.y;
+                    macchina.moveCar(matrice, (righe - 1), (colonne - 1));
+                    macchina.showCar(matrice);
+                    if(pg.y == vy && pg.x > vx)
+                    {
+                        if(macchina.x > vx)
+                        {
+                            if(pg.x <= macchina.x)
+                                morto = true;
+                        } else {
+                            morto = true;
+                        }
+                    }
+                }
+            } while(pg.y > 0 && !morto);
+            if(morto)
+            {
+                Console.WriteLine("Sei stato preso sotto...");
+            } else {
+                Console.WriteLine("Sei passato!");
+            }
+        }
+
+        static void stampaMappa(string[,] matrice, int righe, int colonne)
+        {
+            for(int i=0; i<righe; i++)
+            {
+                for(int j=0; j<colonne; j++)
+                {
                     Console.Write(matrice[i,j]);
                 }
                 Console.Write("\n");
--- Esercizio/car.cs
+++ Esercizio/car.cs
@@ -2,16 +2,49 @@ using System;
 
 namespace Esercizio
 {
-    public class car
+    public class Persona
+    {
+        public int y;
+        public int x;
+        public Persona(int righe, int colonne)
+        {
+            y = righe;
+            x = (colonne/2);
+        }
+        public void showPersona(string[,] matrice)
+        {
+            matrice[y,x] = " * ";
+        }
+    }
+    public class Car
     {
         int velocità;
-        int altezza;
-        int posizione;
-        public car()
+        public int y;
+        public int x;
+        public Car()
         {
             Random rand = new Random();
-            velocità = rand.Next(1,5);
-            altezza = rand.Next(10);
+            velocità = rand.Next(1,6);
+            y = rand.Next(9);
+            x = 0;
+        }
+
+        public void moveCar(string[,] matrice, int righe, int colonne)
+        {
+            matrice[y, x] = " - ";
+            if(((x + velocità) < colonne))
+            {
+                x = (x + velocità);
+            } else {
+                Random rand = new Random();
+                y = rand.Next(righe);
+                x = 0;
+            }
+        }
+
+        public void showCar(string[,] matrice)
+        {
+            matrice[y,x] = " > ";
         }
     }
 }
